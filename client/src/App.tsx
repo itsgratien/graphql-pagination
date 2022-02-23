ff006586f80eb869b4feaeb579e993108c9f5088
itsgratien <gracian2020@gmail.com>
2022-02-23 13:53:59 +0200

    setup sass

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import style from './App.module.scss';
+import classname from 'classnames';
 
 const App = () => {
-  return <div className="App">
-    <span className='font-bold'>welcome</span>
-  </div>;
+  return (
+    <div className={classname('container mx-auto', style.app)}>
+      <span className="font-bold">welcome</span>
+    </div>
+  );
 };
 
 export default App;
